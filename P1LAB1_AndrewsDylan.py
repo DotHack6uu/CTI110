47bb872a978c6fe04ae3d4e61459838987733dd3
DotHack6uu <andrewsd4672@student.faytechcc.edu>
2024-09-16 13:39:08 -0400

    Delete P1LAB1_AndrewsDylan.py

--- P1LAB1_AndrewsDylan.py
+++
@@ -1,8 +0,0 @@
-# Dylan Andrews
-# 16SEP24
-# P1LAB1
-# This program asks for the user to input their first and last name, then displays a welcome message.
-first_name = input("Please enter your first name: ")
-last_name = input("Please enter your last name: ")
-print(f"Hello, {first_name} {last_name}! Welcome to CTI-110")
-
