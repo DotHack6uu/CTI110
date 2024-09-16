80ccf0af3edb319b4b446e81bc83fd449845dd15
DotHack6uu <andrewsd4672@student.faytechcc.edu>
2024-09-16 13:39:47 -0400

    Add files via upload
    First-ish submission.

---
+++ P1LAB1_AndrewsDylan.py
@@ -0,0 +1,8 @@
+# Dylan Andrews
+# 16SEP24
+# P1LAB1
+# This program asks for the user to input their first and last name, then displays a welcome message.
+first_name = input("Please enter your first name: ")
+last_name = input("Please enter your last name: ")
+print(f"Hello, {first_name} {last_name}! Welcome to CTI-110")
+
